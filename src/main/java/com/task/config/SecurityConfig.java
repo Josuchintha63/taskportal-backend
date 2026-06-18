fd9fead6a4defd36502ed653409b816e44cd8c0d
Uppara Hemanth <upparahemanth3@email.com>
2026-06-18 11:40:22 +0530

    Add Vercel domain to CORS allowed origins

--- src/main/java/com/task/config/SecurityConfig.java
+++ src/main/java/com/task/config/SecurityConfig.java
@@ -42,7 +42,10 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:5173"));
+		config.setAllowedOrigins(List.of(
+			    "http://localhost:5173",
+			    "https://taskportal-frontend-nine.vercel.app"
+			));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
